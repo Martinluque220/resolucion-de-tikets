6ff4af51bc12736b155d2adad1e7b8277cb14701
Martinluque220 <martinluque2205@gmail.com>
2025-07-11 02:34:08 +0200

    Update functions.ajax.js

--- bootstrap/js/functions.ajax.js
+++ bootstrap/js/functions.ajax.js
@@ -1,56 +1,41 @@
 $(document).ready(function(){
- 
-// Login Ajax:
-// Php mysql Ajax,
-// Copyright 2015 bloguero-ec.
-// Usese cómo mas le convenga no elimine estas líneas (http://www.bloguero-ec.com)
- 
-// tiempo de el slide en segundos  
+
     var timeSlide = 1000;
-//posicionamos el puntero en el campo de usuario
+
     $('#login_username').focus();
-//posicionamos el tiempo de ajax en cero
     $('#timer').hide(0);
-//por el momento no mostramos el ajax
     $('#timer').css('display','none');
-//damos clic en boton inicio de sesion
+
     $('#login_userbttn').click(function(){
-//posicionamos el ajax de cero a 300 en fade
         $('#timer').fadeIn(300);
-//mostramos las clases creadas dentro del css para mostrar los mensajes
         $('.box-info, .box-success, .box-alert, .box-error').slideUp(timeSlide);
-//configuramos y creamos la condicion
+
         setTimeout(function(){
-            if ( $('#login_username').val() != "" && $('#login_userpass').val() != "" ){
-                 
+            if ($('#login_username').val() != "" && $('#login_userpass').val() != "") {
                 $.ajax({
                     type: 'POST',
                     url: 'log.inout.ajax.php',
                     data: 'login_username=' + $('#login_username').val() + '&login_userpass=' + $('#login_userpass').val(),
-                     
-//si la sesion se inicia correctamente presentamos el mensaje
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    
                     success:function(msj){
-                        if ( msj == 1 ){
-						//if ( msj == 'administrador' ){
+                        if (msj == 1) {
                             $('#alertBoxes').html('<div class="box-success"></div>');
                             $('.box-success').hide(0).html('Espera un momento…');
                             $('.box-success').slideDown(timeSlide);
                             setTimeout(function(){
-                                window.location.href = ".";
-                            },(timeSlide + 500));
-							
-                        }
-                         
-//caso contrario los datos son incorrectos
-                        else{
+                                window.location.href = "index.php";
+                            }, (timeSlide + 500));
+                        } else {
                             $('#alertBoxes').html('<div class="box-error"></div>');
                             $('.box-error').hide(0).html('Lo sentimos, pero los datos son incorrectos: ' + msj);
                             $('.box-error').slideDown(timeSlide);
-							
                         }
                         $('#timer').fadeOut(300);
                     },
-//si se pierden los datos presentamos error de ejecucion.
+
                     error:function(){
                         $('#timer').fadeOut(300);
                         $('#alertBoxes').html('<div class="box-error"></div>');
@@ -58,24 +43,17 @@ $(document).ready(function(){
                         $('.box-error').slideDown(timeSlide);
                     }
                 });
-                 
-            }
-             
-//caso cantrario si los campos estan vacios debemos llenarlos
-            else{
+            } else {
                 $('#alertBoxes').html('<div class="box-error"></div>');
                 $('.box-error').hide(0).html('Los campos estan vacios');
                 $('.box-error').slideDown(timeSlide);
                 $('#timer').fadeOut(300);
             }
-        },timeSlide);
-         
+        }, timeSlide);
+
         return false;
-         
     });
-     
-     
-//funcion para destruir la sesion iniciada 
+
     $('#sessionKiller').click(function(){
         $('#timer').fadeIn(300);
         $('#alertBoxes').html('<div class="box-success"></div>');
@@ -83,47 +61,44 @@ $(document).ready(function(){
         $('.box-success').slideDown(timeSlide);
         setTimeout(function(){
             window.location.href = "logout.php";
-        },2500);
+        }, 2500);
     });
-     
 
-//REGISTRO
-$('#rec_userbttn').click(function(){
-//posicionamos el ajax de cero a 300 en fade
+    // REGISTRO
+    $('#rec_userbttn').click(function(){
         $('#timer').fadeIn(300);
-//mostramos las clases creadas dentro del css para mostrar los mensajes
         $('.box-info, .box-success, .box-alert, .box-error').slideUp(timeSlide);
-//configuramos y creamos la condicion
+
         setTimeout(function(){
-            if ( $('#rec_username').val() != "" && $('#rec_userpass').val() != "" && $('#rec_userpass').val() == $('#rec_userpass_1').val() && $('#rec_email').val() != "" ){
-                 
+            if (
+                $('#rec_username').val() != "" &&
+                $('#rec_userpass').val() != "" &&
+                $('#rec_userpass').val() == $('#rec_userpass_1').val() &&
+                $('#rec_email').val() != ""
+            ) {
                 $.ajax({
                     type: 'POST',
                     url: 'log.inout.ajax.php',
-                    data: 'rec_username=' + $('#rec_username').val() + '&rec_userpass=' + $('#rec_userpass').val() + '&rec_email=' + $('#rec_email').val(),
-                     
-//si la sesion se inicia correctamente presentamos el mensaje
+                    data: 'rec_username=' + $('#rec_username').val() +
+                          '&rec_userpass=' + $('#rec_userpass').val() +
+                          '&rec_email=' + $('#rec_email').val(),
+
                     success:function(msj){
-                        if ( msj == 1 ){
-						    $('#alertBoxes').html('<div class="box-success"></div>');
+                        if (msj == 1) {
+                            $('#alertBoxes').html('<div class="box-success"></div>');
                             $('.box-success').hide(0).html('Espera un momento…');
                             $('.box-success').slideDown(timeSlide);
                             setTimeout(function(){
-                                window.location.href = ".";
-                            },(timeSlide + 500));
-							
-                        }
-                         
-//caso contrario los datos son incorrectos
-                        else{
+                                window.location.href = "index.php"; // REDIRECCIÓN CORREGIDA
+                            }, (timeSlide + 500));
+                        } else {
                             $('#alertBoxes').html('<div class="box-error"></div>');
                             $('.box-error').hide(0).html('Lo sentimos, pero los datos son incorrectos: ' + msj);
                             $('.box-error').slideDown(timeSlide);
-							
                         }
                         $('#timer').fadeOut(300);
                     },
-//si se pierden los datos presentamos error de ejecucion.
+
                     error:function(){
                         $('#timer').fadeOut(300);
                         $('#alertBoxes').html('<div class="box-error"></div>');
@@ -131,22 +106,15 @@ $('#rec_userbttn').click(function(){
                         $('.box-error').slideDown(timeSlide);
                     }
                 });
-                 
-            }
-             
-//caso cantrario si los campos estan vacios debemos llenarlos
-            else{
+            } else {
                 $('#alertBoxes').html('<div class="box-error"></div>');
                 $('.box-error').hide(0).html('Los campos estan vacios o Faltan Datos');
                 $('.box-error').slideDown(timeSlide);
                 $('#timer').fadeOut(300);
             }
-        },timeSlide);
-         
+        }, timeSlide);
+
         return false;
-         
     });
-     
-    
-	 
-	 });
+
+});
