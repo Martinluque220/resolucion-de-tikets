bdfb29c07168a5976b08ea8f30fe62804f8522c7
Martinluque220 <martinluque2205@gmail.com>
2025-07-11 02:28:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');  // O la contraseña si usás una
-define('DB_NAME', 'biblio_t1');  // O el nombre real de tu base
+define('DB_PASS', '');
+define('DB_NAME', 'biblio_t1');
 
 function conectar() {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -10,5 +10,9 @@ function conectar() {
         die('Error de conexión: ' . $conn->connect_error);
     }
     return $conn;
+}
+
+function formatDate($date) {
+    return date('g:i a', strtotime($date));
 }
 ?>
